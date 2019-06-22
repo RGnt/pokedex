d1b8cce557dd621a31743ca9ed3cccd5d1d2634f
Riku-Juhani Niemela <rjniem@gmail.com>
2019-06-22 21:31:24 +0300

    Updates

---
+++ precache-manifest.b1c34d91de6a4b0cb934b112057269e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c0bf4c4f5fd5f84bc2251e2d0b35c5",
+    "url": "/RGnt/pokedex/index.html"
+  },
+  {
+    "revision": "8ad8e3ec8dc1370ac613",
+    "url": "/RGnt/pokedex/static/js/2.a5a7cf24.chunk.js"
+  },
+  {
+    "revision": "a8f0e80d40eb61a3c4bf",
+    "url": "/RGnt/pokedex/static/js/main.ece5c1df.chunk.js"
+  },
+  {
+    "revision": "3d2dc46ab553675fa09b",
+    "url": "/RGnt/pokedex/static/js/runtime~main.cc514999.js"
+  }
+]);
