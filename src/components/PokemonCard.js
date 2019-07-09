a248497af936db7c36b9bbb042177db7eb1e4c75
Riku-Juhani Niemela <rjniem@gmail.com>
2019-07-09 16:10:35 +0300

    Fonts and some other minor tweaks to cards

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -39,22 +39,22 @@ export default class PokemonCard extends React.Component {
     }
 
     // Refactor: move to helper file
-    createGradient = (color1, color2) => ({"background": `linear-gradient( -45deg, ${color1}, ${color2})`})
+    createGradient = (color1, color2) => ({ "background": `linear-gradient( -45deg, ${color1}, ${color2})` })
     capitalize = (input) => {
         return input.charAt(0).toUpperCase() + input.slice(1)
     }
 
     createColorName = (type) => {
-        return "color"+this.capitalize(type);
+        return "color" + this.capitalize(type);
     }
     createHeaderColor = (types) => {
-        if(types.length > 1) {
+        if (types.length > 1) {
             //console.log(this.createGradient(variables[this.createColorName(types[0].type.name)], variables[this.createColorName(types[1].type.name)]))
             return this.createGradient(variables[this.createColorName(types[0].type.name)], variables[this.createColorName(types[1].type.name)]);
         } else {
             //console.log("should be single color")
             //console.log({"background": `${this.createColorName(types[0].type.name)}`})
-            return ({"background": variables[this.createColorName(types[0].type.name)]})
+            return ({ "background": variables[this.createColorName(types[0].type.name)] })
         }
     }
     // Refactor: end
@@ -65,18 +65,17 @@ export default class PokemonCard extends React.Component {
 
                 <div className="card">
                     <div className="cardInner" style={this.createHeaderColor(this.state.pokemon.types)}>
-                    <div className="cardHeader">
-                        <h2 className="cardHeaderName">{this.capitalize(this.state.pokemon.name)}</h2>
-                    </div>
+                        <div className="cardHeader">
+                            <h2 className="cardHeaderName">{this.capitalize(this.state.pokemon.name)}</h2>
+                        </div>
                         <div className="cardBody">
                             <div className="cardImage" >
                                 <img src={this.state.pokemon.sprites.front_default} alt={this.state.pokemon.name} />
                             </div>
 
                             <div className="cardTypes">
-                                <div>{this.state.pokemon.types.map((type) => <span key={type.type.name} className="typeFlair">{type.type.name}</span>)}</div>
-                            </div>
-                        <div class="set"><h3 className="cardHeaderNumber">Gen 1: {`#${this.state.pokemon.id}`}/151</h3></div>
+                                <div>{this.state.pokemon.types.map((type) => <span key={type.type.name} className="typeFlair" style={{ "background": `${variables[this.createColorName(type.type.name)]}` }}>{type.type.name}</span>)}</div>                            </div>
+                            <div class="set"><h3 className="cardHeaderNumber">Gen 1: {`#${this.state.pokemon.id}`}/151</h3></div>
                         </div>
                     </div>
                 </div>
